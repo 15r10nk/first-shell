7316ed0a63a60ad5415f96ddd24274f6d8b95dfb
Frank Hoffmann <15r10nk-git@polarbit.de>
2023-01-18 14:01:59 +0100

    Updates

---
+++ _app/immutable/chunks/2-162465e3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-df8a2dac.js";export{t as component};
--- _app/immutable/chunks/2-4c9550f4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-30e0bbf4.js";export{t as component};
