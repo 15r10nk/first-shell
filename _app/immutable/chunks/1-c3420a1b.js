fce2e493452eba93d599869222d9b8ceaba00ece
Frank Hoffmann <15r10nk-git@polarbit.de>
2023-01-18 22:05:52 +0100

    Updates

--- _app/immutable/chunks/1-c3420a1b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-20d01292.js";export{t as component};
---
+++ _app/immutable/chunks/2-e924782b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-b69d4b2f.js";export{t as component};
